5bc4273f7e6f928b0e59e23185a7064211f87afe
cassandrasortais <cassandrasortais@localhost>
2024-11-20 13:03:22 +0100

    Création de la class Pion : J'ai implémenté la classe Pion pour représenter un élément du jeu MasterMind. Chaque pion a une couleur, représentée par un caractère (par exemple, 'R' pour rouge). J'ai ajouté un constructeur pour initialiser la couleur et un accesseur pour la récupérer. La méthode toString() permet d'afficher la couleur sous forme de chaîne. J'ai ensuite testé la classe en créant des objets Pion et vérifié que l'affichage et les accesseurs fonctionnaient correctement.

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,6 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+TP MINI PROJET - MASTERMIND
+TDC
+SORTAIS CASSANDRA
+PREVOT NINON 
+20 Novembre 2024
+ 
  */
 package mastermind;
 
@@ -13,8 +17,25 @@ public class Mastermind {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+
+       public static void main(String[] args) {
+        // TODO code application logic here
+
+        // Création de quelques pions de différentes couleurs
+        Pion pionRouge = new Pion('R');
+        Pion pionBleu = new Pion('B');
+        Pion pionVert = new Pion('G');
+
+        // Test des accesseurs
+        System.out.println("Couleur du pion rouge : " + pionRouge.getCouleur()); // Affiche 'R'
+        System.out.println("Couleur du pion bleu : " + pionBleu.getCouleur());   // Affiche 'B'
+        System.out.println("Couleur du pion vert : " + pionVert.getCouleur());   // Affiche 'G'
+
+        // Test de la méthode toString()
+        System.out.println("Représentation du pion rouge : " + pionRouge); // Affiche 'R'
+        System.out.println("Représentation du pion bleu : " + pionBleu);   // Affiche 'B'
+        System.out.println("Représentation du pion vert : " + pionVert);   // Affiche 'G'
+    }
 }
+
---
+++ src/mastermind/Pion.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind;
+
+/**
+ *
+ * @author cassandrasortais
+ */
+public class Pion {
+
+    // Attribut représentant la couleur du pion
+    private Character couleur;
+
+    // Constructeur pour initialiser un pion avec une couleur donnée
+    public Pion(Character couleur) {
+        this.couleur = couleur;
+    }
+
+    // Accesseur pour obtenir la couleur du pion
+    public Character getCouleur() {
+        return couleur;
+    }
+
+    // Redéfinition de la méthode toString() pour afficher la couleur du pion
+    @Override
+    public String toString() {
+        return couleur.toString();
+    }
+}
